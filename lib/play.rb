89200c268d7749d4de410d987aa18f6e7c5b4c04
Tom Storm <tstorm@invoca.com>
2018-01-10 21:44:38 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -25,8 +25,8 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
+  user_input = gets.strip
+  index = input_to_index(user_input)
   if valid_move?(board, index)
     move(board, index)
     display_board(board)
